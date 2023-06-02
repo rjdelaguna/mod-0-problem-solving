8995eefc5aa6fe35bd0f090034a12ef8b78ef639
Robert deLaguna <rjdelaguna@yahoo.com>
2023-06-02 11:45:16 -0700

    Adjust notes in challenge 6

--- challenge6.rb
+++ challenge6.rb
@@ -1,7 +1,8 @@
 # Given a sentence with only lowercase letters, print the same sentence with the first letter of every word capitalized. For example, if you were given "Turing is the best", return "Turing Is The Best" instead!
 
 # This requires the creation of a variable that holds the text string.
-# Then use method titleize to capitalize each word.
+# Then use methods I discovered .split, .map, and .join
+# split takes the string a seperates each word into it's own array element, then map is used to call the capitalize method to each element in the array, then join is used to bring the elements back into a string but you must call your own separator (" ") or else it will return as one word.
 
 sentence = "Turing is the best."
 def caps(strings)
